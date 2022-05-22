094f9633cd2db8e4892f2eaf38d96a6b4d3ca191
FU CHEN KUO <101970281+FiveFiveKUO@users.noreply.github.com>
2022-05-22 22:27:30 +0800

    Add files via upload

---
+++ DATACOMPARISON2.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+import subprocess
+
+class MyFrame(tk.LabelFrame):
+
+    def __init__(self, root):
+        tk.LabelFrame.__init__(self, root, text="資料比對工具")
+        self.b1 = tk.Button(self, text="執行", command=self.DATA)
+        # tk.X
+        self.b1.pack(expand=True, fill=tk.BOTH, pady=10, padx=5)
+        self.result = tk.Label(self, text="按上面按鈕進行資料比對...")
+        self.result.pack(expand=True, fill=tk.BOTH, pady=10, padx=5)
+
+    def DATA(self):
+        DATA1 = open("DATA1.txt").read().split()
+        DATA2 = open("DATA2.txt").read().split()
+
+        cnt = 0
+        for i, policynum in enumerate(DATA2):
+            if policynum in DATA1:
+                cnt = cnt + 1
+                print("第", i + 1, "張POLICY", policynum, "!")
+
+        print("相同POLICY共", cnt, "張")
+        # 第二時間
+        self.result["text"] = (("相同POLICY共"), cnt, ("張"))
+        with open("FINALDATA.txt", "w") as f1, open("err.txt", "w") as f2:
+            result = subprocess.run(stdout=f1, stderr=f2)
+            print(result)
+
+window = tk.Tk()
+window.geometry("500x500+100+100")
+f1 = MyFrame(window)
+f1.pack(side=tk.LEFT, expand=True, fill=tk.BOTH, padx=20, pady=20)
+window.mainloop()
+
+
+
+
+
